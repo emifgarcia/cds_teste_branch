75e669f43aa4358a37f20b93569d9d9cdcb521c1
Marcelo Figueiredo Garcia <marcelof.garcia@yahoo.com.br>
2024-02-12 09:01:55 -0300

    adicionado operacao

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 def gether_data():
-    n1 = int(input("Primeiro valor: "))
-    n2 = int(input("Segundo valor: "))
-    return n1, n2
+    n1 = input("Primeiro valor: ")
+    n2 = input("Segundo valor: ")
+    op = input("Operação: ")
+    return n1, n2, op
 
 def main():
-    n1, n2 = gether_data()
-    print(2**2)
+    n1, n2, op = gether_data()
+    print(eval(n1+op+n2))
     return None
 
 if __name__ == "__main__":
