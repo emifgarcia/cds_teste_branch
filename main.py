34f98b5a71019a6b50d04aeee9a6c49ec69937cb
Marcelo Figueiredo Garcia <marcelof.garcia@yahoo.com.br>
2024-02-12 09:49:07 -0300

    ajustado maneira de recuperar operacao

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 def gether_data():
     n1 = input("Primeiro valor: ")
     n2 = input("Segundo valor: ")
-    op = input("Operação: ")
+
+    op = get_operation()
+    
     return n1, n2, op
 
 def main():
     n1, n2, op = gether_data()
     print(eval(n1+op+n2))
     return None
+
+def get_operation():
+    op = input("Operação: ")
+
+    return op
 
 if __name__ == "__main__":
     main()
